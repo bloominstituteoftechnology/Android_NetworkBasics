b395a467511615ba91ee8416c31c88e567a4ac33
Jacob Seppanen <jbseppanen@gmail.com>
2018-11-27 14:51:24 -0800

    Create XkcdComic data model class.

--- app/src/main/java/com/example/jacob/android_networkbasics/NetworkAdapter.java
+++ app/src/main/java/com/example/jacob/android_networkbasics/NetworkAdapter.java
@@ -1,6 +1,9 @@
 package com.example.jacob.android_networkbasics;
 
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,13 +13,13 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 public class NetworkAdapter {
-
+    final static int TIMEOUT = 3000;
 
     public static String httpRequest(String urlString) {
         String result = "";
         InputStream stream = null;
         HttpURLConnection connection = null;
-        final int TIMEOUT = 3000;
+
 
         try {
             URL url = new URL(urlString);
@@ -31,7 +34,7 @@ public class NetworkAdapter {
                     BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
                     StringBuilder builder = new StringBuilder();
                     String line;
-                    while((line = reader.readLine()) != null) {
+                    while ((line = reader.readLine()) != null) {
                         builder.append(line);
                     }
                     result = builder.toString();
@@ -46,8 +49,7 @@ public class NetworkAdapter {
         } catch (IOException e) {
             e.printStackTrace();
             return "";
-        }
-        finally {
+        } finally {
             if (stream != null) {
                 try {
                     stream.close();
@@ -63,4 +65,41 @@ public class NetworkAdapter {
     }
 
 
-}
+    public static Bitmap httpImageRequest(String urlString) {
+        Bitmap resultImage = null;
+        InputStream stream = null;
+        HttpURLConnection connection = null;
+
+        URL url = null;
+        try {
+            url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(TIMEOUT);
+            connection.setConnectTimeout(TIMEOUT);
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                stream = connection.getInputStream();
+                if (stream != null) {
+                    resultImage = BitmapFactory.decodeStream(stream);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return resultImage;
+        }
+    }
