f62017c9cbf8a5c755c99fd16467ba0d6c8d4e07
Patrick Martin <patrickmartin88@gmail.com>
2018-11-27 15:07:52 -0800

    Finishing up XkcdComicDao before rewatching the rest of the lecture video that I missed.

--- app/src/main/java/com/example/patrickjmartin/network_basics/XkcdDao.java
+++ app/src/main/java/com/example/patrickjmartin/network_basics/XkcdDao.java
@@ -1,4 +1,41 @@
 package com.example.patrickjmartin.network_basics;
 
 public class XkcdDao {
+
+    private final static String BASE_URL = "https://xkcd.com/";
+    private final static String URL_ENDING = "info.0.json";
+    private final static String RECENT_COMIC = BASE_URL + URL_ENDING;
+    private final static String SPECIFIC_COMIC = BASE_URL + "%d/" + URL_ENDING;
+
+    public static XkcdComic getRecentComic() {
+        XkcdComic xkcdComic = null;
+
+        //stuff
+
+        return xkcdComic;
+    }
+
+    public static XkcdComic getRandomComic() {
+        XkcdComic xkcdComic = null;
+
+        //stuff
+
+        return xkcdComic;
+    }
+
+    public static XkcdComic getNextComic() {
+        XkcdComic xkcdComic = null;
+
+        //stuff
+
+        return xkcdComic;
+    }
+
+    public static XkcdComic getPreviousComic() {
+        XkcdComic xkcdComic = null;
+
+        //stuff
+
+        return xkcdComic;
+    }
 }
