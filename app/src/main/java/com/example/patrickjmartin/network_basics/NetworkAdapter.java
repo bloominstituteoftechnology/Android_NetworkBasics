0cca5dbc24845dc3211042aee68e302e8e6d16e7
Patrick Martin <patrickmartin88@gmail.com>
2018-11-27 14:28:45 -0800

    Implemented the request for an Image Response. Bitmap will properly be delivered.

--- app/src/main/java/com/example/patrickjmartin/network_basics/NetworkAdapter.java
+++ app/src/main/java/com/example/patrickjmartin/network_basics/NetworkAdapter.java
@@ -1,5 +1,9 @@
 package com.example.patrickjmartin.network_basics;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.ProgressBar;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -61,5 +65,43 @@ public class NetworkAdapter {
             }
         }
         return result;
+    }
+
+    public static Bitmap httpImageRequest(String urlString){
+        Bitmap comic = null;
+        InputStream stream = null;
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(TIMEOUT);
+            connection.setConnectTimeout(TIMEOUT);
+
+            connection.connect();
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                stream = connection.getInputStream();
+                if (stream != null) {
+                    comic = BitmapFactory.decodeStream(stream);
+                }
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } return comic;
     }
 }
