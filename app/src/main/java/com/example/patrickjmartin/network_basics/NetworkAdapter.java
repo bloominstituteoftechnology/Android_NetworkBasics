7282b329cb5bf27f2c8c5644e62d8891b1e1a94f
Patrick Martin <patrickmartin88@gmail.com>
2018-11-27 13:36:51 -0800

    Implemented NetworkAdapter class.

---
+++ app/src/main/java/com/example/patrickjmartin/network_basics/NetworkAdapter.java
@@ -0,0 +1,61 @@
+package com.example.patrickjmartin.network_basics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetworkAdapter {
+
+    public static String httpRequest (String urlString) {
+        String result = "";
+        HttpURLConnection connection = null;
+        InputStream stream = null;
+
+        try {
+            URL url = new URL(urlString);
+            connection = (HttpURLConnection) url.openConnection();
+
+            connection.connect();
+
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                stream = connection.getInputStream();
+                if (stream != null) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                    StringBuilder builder = new StringBuilder();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        builder.append(line);
+                        line = reader.readLine();
+                    }
+                    result = builder.toString();
+                }
+
+            } else {
+                result = String.valueOf(responseCode);
+            }
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+
+            if (stream != null) {
+                try {
+                    stream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
+}
