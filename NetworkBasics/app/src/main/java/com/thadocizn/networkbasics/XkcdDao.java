7b42ef957d8f500072020131280550680f0f0fde
ThaDocIzN <rodan72@msn.com>
2018-11-27 18:25:34 -0600

    added a couple of constants to the dao

--- NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDao.java
+++ NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDao.java
@@ -1,4 +1,7 @@
 package com.thadocizn.networkbasics;
 
 public class XkcdDao {
+    private static final String BASE_URL = "http://xkcd.com/";
+    private static final String IMAGE_URL = "https://xkcd.com/info.0.json";
+
 }
