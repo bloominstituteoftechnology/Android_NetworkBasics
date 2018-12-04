cca1ac2e8b04ab816aab1d7d87fb5c945e19df78
ThaDocIzN <rodan72@msn.com>
2018-12-04 08:52:40 -0600

    fixed the bug in dao

--- NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDao.java
+++ NetworkBasics/app/src/main/java/com/thadocizn/networkbasics/XkcdDao.java
@@ -36,80 +36,20 @@ public class XkcdDao {
 
     public static XkcdComic getNextComic(XkcdComic comic){
         int next = comic.getNum() + 1;
-        String url = SPECIFIC_COMIC.replace("%d/", Integer.toString(next));
+        String url = SPECIFIC_COMIC.replace("%d", Integer.toString(next));
         return getComic(url);
     }
 
     public static XkcdComic getPreviousComic(XkcdComic comic){
         int previous = comic.getNum() - 1;
-        String url = SPECIFIC_COMIC.replace("%d/", Integer.toString(previous));
+        String url = SPECIFIC_COMIC.replace("%d", Integer.toString(previous));
         return getComic(url);
     }
 
     public static XkcdComic getRandomComic(){
         int random = ((int)(Math.random() * maxComicNumber) + 1);
-        String url = SPECIFIC_COMIC.replace("%d/", Integer.toString(random));
+        String url = SPECIFIC_COMIC.replace("%d", Integer.toString(random));
         return getComic(url);
     }
 
 }
-
-/*
-public class XkcdDao {
-    private static final String BASE_URL = "https://xkcd.com/";
-    private static final String URL_ENDING = "info.0.json";
-    private static final String CURRENT_URL = BASE_URL + URL_ENDING;
-    private static final String SPECIFIC_COMIC = BASE_URL + "%d/" + URL_ENDING;
-    private static XkcdComic current;
-    public static final int MAX = 2077;
-
-    private static XkcdComic getComic(String url){
-        XkcdComic comic = null;
-        try {
-            String strUrl = NetworkAdapter.httpGetRequest(url);
-            JSONObject jsComic = new JSONObject(NetworkAdapter.httpGetRequest(url));
-            comic = new XkcdComic(jsComic);
-
-            String urlString = comic.getImg();
-                Bitmap bitmap;
-                bitmap = NetworkAdapter.httpImageRequest(comic.getImg());
-                    comic.setImage(bitmap);
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return comic;
-    }
-
-    public static XkcdComic getRecentComic() {
-        XkcdComic comic = null;
-        comic = getComic(CURRENT_URL);
-        return comic;
-    }
-
-    public static XkcdComic getNextComic(XkcdComic comic) {
-        int num = current.getNum() +1;
-            String url = SPECIFIC_COMIC.replace("%d/", Integer.toString(num));
-            comic = getComic(url);
-        return comic;
-    }
-
-    public static XkcdComic getPreviousComic() {
-        XkcdComic comic = null;
-        int num = current.getNum();
-            num -= 1;
-            String url = SPECIFIC_COMIC.replace("%d/", Integer.toString(num));
-            comic = getComic(url);
-
-        return comic;
-    }
-
-    public static XkcdComic getRandomComic(){
-        XkcdComic comic = null;
-        String randomNumber = String.valueOf((Math.round(Math.random()* MAX + 1)));
-        String url = SPECIFIC_COMIC.replace("%d/", randomNumber);
-        comic = getComic(url);
-        return comic;
-    }
-}
-*/
