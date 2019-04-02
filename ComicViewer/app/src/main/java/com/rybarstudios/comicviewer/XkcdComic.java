9afe6febb4ea7bc791135205d759dfc629e8a745
Christian Rybarczyk <christian.j.rybarczyk@gmail.com>
2019-04-02 16:16:57 -0400

    Deleted setters in XkcdComic

--- ComicViewer/app/src/main/java/com/rybarstudios/comicviewer/XkcdComic.java
+++ ComicViewer/app/src/main/java/com/rybarstudios/comicviewer/XkcdComic.java
@@ -87,88 +87,44 @@ public class XkcdComic {
         return month;
     }
 
-    public void setMonth(String month) {
-        this.month = month;
-    }
-
     public String getLink() {
         return link;
-    }
-
-    public void setLink(String link) {
-        this.link = link;
     }
 
     public String getYear() {
         return year;
     }
 
-    public void setYear(String year) {
-        this.year = year;
-    }
-
     public String getNews() {
         return news;
-    }
-
-    public void setNews(String news) {
-        this.news = news;
     }
 
     public String getSafeTitle() {
         return safeTitle;
     }
 
-    public void setSafeTitle(String safeTitle) {
-        this.safeTitle = safeTitle;
-    }
-
     public String getTranscript() {
         return transcript;
-    }
-
-    public void setTranscript(String transcript) {
-        this.transcript = transcript;
     }
 
     public String getAlt() {
         return alt;
     }
 
-    public void setAlt(String alt) {
-        this.alt = alt;
-    }
-
     public String getImg() {
         return img;
-    }
-
-    public void setImg(String img) {
-        this.img = img;
     }
 
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
     public String getDay() {
         return day;
     }
 
-    public void setDay(String day) {
-        this.day = day;
-    }
-
     public int getNum() {
         return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
     }
 
     public Bitmap getBitMap() {
